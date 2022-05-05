58c7aa45354b3016c3a5905cdca20c8ed93a5b41
luv-jeri <luvvjeri@gmail.com>
2022-05-05 18:55:45 +0530

    -

--- src/pages/count.jsx
+++ src/pages/count.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-export default function Count() {
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.count);
-  const user = useSelector((state) => state.user);
 
+import { useSelector, useDispatch } from 'react-redux';
+import countAction from '../actions/count';
+import userAction from '../actions/users';
+const Count = () => {
+  const dispatch = useDispatch();
+
+  const { inc } = countAction;
+  const { fetchAwait, fetchMiddleware } = userAction;
+
+  const [num, setNum] = React.useState(0);
+
+  const count = useSelector((state) => state.count);
+  const { name } = useSelector((state) => state.name);
+  const users = useSelector((state) => state.users);
+  
+  // dispatch({ type: 'inc' });
+
+  // console.log(test);
+  // const user = useSelector((state) => state.user);
+  console.log(users);
   return (
     <div
       style={{
@@ -13,38 +28,33 @@ export default function Count() {
         flexDirection: 'column',
       }}
     >
-      Name {user?.name || 'Guest'}
-      <br/>
-      isLogin {user?.isLogged ? 'true' : 'false'}
-      <h1>Count : {count}</h1>
+      {users.map((user) => {
+        return <div key={user.id}>{user.name}</div>;
+      })}
+      {/* Name {user?.name || 'Guest'}
+      <br />
+      isLogin {user?.isLogged ? 'true' : 'false'} */}
+      <h1>Count : {count.value}</h1>
+      <h1>Name : {name}</h1>
+      <input type='text' value={num} onChange={(e) => setNum(e.target.value)} />
       <button
-        onClick={() => {
-          dispatch({ type: 'inc' });
+        onClick={async () => {
+          dispatch(inc(4));
         }}
       >
         INC
       </button>
+      <button onClick={() => {}}>DEC</button>
       <button
-        onClick={() => {
-          dispatch({ type: 'dec' });
+        onClick={async () => {
+          // dispatch(await fetchAwait());
+          dispatch(fetchMiddleware());
         }}
       >
-        DEC
-      </button>
-      <button
-        onClick={() => {
-          dispatch({ type: 'set' });
-        }}
-      >
-        Set
-      </button>
-      <button
-        onClick={() => {
-          dispatch({ type: 'delete' });
-        }}
-      >
-        delete
+        FETCH
       </button>
     </div>
   );
-}
+};
+
+export default React.memo(Count);

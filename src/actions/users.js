d80f74b22da51ac406f6e92c2104b3241987dd46
luv-jeri <luvvjeri@gmail.com>
2022-05-05 18:57:48 +0530

    -

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 
+const FETCH_USERS = 'FETCH_USERS';
+const SET_USER = 'SET_USER';
+
 const fetchAwait = async () => {
   const res = await axios.get('https://jsonplaceholder.typicode.com/users');
-  return { type: 'FETCH_USERS', payload: res.data };
+  return { type: FETCH_USERS, payload: res.data };
 };
 
 const fetchMiddleware = () => {
   return async (dispatch) => {
     const res = await axios.get('https://jsonplaceholder.typicode.com/users');
 
-    dispatch({ type: 'SET_USER', payload: res.data });
+    dispatch({ type: SET_USER, payload: res.data });
   };
 };
 
